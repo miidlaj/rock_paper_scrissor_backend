662fd9e6803f164e146d96ed3e81db74403c078b
miidlaj <mumidlaj@gmail.com>
2023-04-01 11:52:45 +0530

    First

---
+++ index.js
@@ -0,0 +1,31 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const roomHandler = require("./roomHandler");
+
+dotenv.config();
+const app = express();
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: { origin: "*" },
+});
+
+const rooms = [];
+
+io.on("connection", (socket) => {
+  console.log("connected", socket.id);
+  roomHandler(io, socket, rooms);
+
+  socket.on("disconnect", () => {
+    console.log("disconnected", socket.id);
+  })
+  socket.on("error", () => {
+    console.log("Error", socket.id);
+  })
+})
+
+
+const port = process.env.PORT || 8080;
+httpServer.listen(port, () => console.log(`Listening on port ${port}`));
